1557dc4fa99bb4469e9278747b352c605f9a8105
wilsonfreitas <wilson.freitas@gmail.com>
2014-04-21 21:57:50 -0300

    Novos pacotes instalados e urlfetch implementado.

--- backend/src/web/rest.py
+++ backend/src/web/rest.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from bs4 import BeautifulSoup
+from google.appengine.api import urlfetch
 
 
-def items(_json):
-    _json({'data': [1, 2, 3]})
+def index(_json):
+	url = 'http://www.cetip.com.br/'
+	result = urlfetch.fetch(url)
+	if result.status_code == 200:
+		soup = BeautifulSoup(result.content)
+		# <span id="ctl00_Banner_lblTaxDI">10,80%</span>
+		# <span id="ctl00_Banner_lblTaxDateDI">(17/04/2014)</span>
+		# soup.find_all(id='ctl00_Banner_lblTaxDI')
+		rate = soup.find_all(id='ctl00_Banner_lblTaxDI')[0].string
+		rate = float(rate.replace('%', '').replace(',', '.'))
+		_json({'CDI': rate})
+	else:
+		_json({})
+
+
+
+
+
